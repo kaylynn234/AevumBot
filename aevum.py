5aa9d6dbd4b56916fa80dc8e58d6f3ef4e8e41bb
Kaylynn <mkaylynn7@gmail.com>
2020-05-10 16:42:07 +1000

    i think it works now

--- aevum.py
+++ aevum.py
@@ -5,6 +5,7 @@ import sys
 import os
 import json
 import aevum_auth
+import traceback
 
 json_path = f"{sys.path[0]}/tz.json"
 
@@ -46,13 +47,13 @@ def ensure_data(member_id_to_find=None):
         if member:
             return member
         else:
-            raise NoTimezonesFoundError("This member has not registered a timezxone yet!")
+            raise NoTimezonesFoundError("This member has not registered a timezone yet!")
 
     return loaded
 
 
 bot = commands.AutoShardedBot(command_prefix="tz ", case_insensitive=True)
-bot.load_extension("Jishaku")
+bot.load_extension("jishaku")
 
 
 @bot.command()
@@ -64,12 +65,13 @@ async def all(ctx):
 
     data = []
     for user_id, tz in loaded.items():
-        readable = getattr(bot.get_user(user_id), "display_name", None) or user_id
-        current_time = pendulum.now(tz=tz).format("dddd DD MMMM YYYY HH:mm:ss")
+        user = bot.get_user(int(user_id))
+        readable = user.display_name if user else user_id
+        current_time = pendulum.now(tz=tz).format("dddd DD MMMM YYYY HH:mm")
         data.append(f"{readable}: {current_time}")
 
     page_data = TimezoneMenu(data, "Timezones for all members")
-    menu = menus.MenuPages(page_data, 180, clear_reactions_after=True)
+    menu = menus.MenuPages(page_data, timeout=180, clear_reactions_after=True)
     await menu.start(ctx)
 
 
@@ -87,7 +89,7 @@ async def user(ctx, *, member: discord.Member):
 
 
 @bot.command(name="set")
-async def set_tz(ctx, *, tz: discord.Member):
+async def set_tz(ctx, *, tz):
     """Sets your timezone. The tz argument should be a TZ database code, which generally boils down to a "Country/City".
     Examples of valid TZ database codes are "Europe/Lisbon", "Pacific/Auckland" and "America/Los_Angeles".
     See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List for more details.
@@ -108,9 +110,15 @@ async def set_tz(ctx, *, tz: discord.Member):
 
 
 @bot.event
+async def on_ready():
+    print("Bot is alive.")
+
+
+@bot.event
 async def on_command_error(ctx, error):
     """Run when shit breaks."""
 
+    error = getattr(error, "original", error)
     if isinstance(error, commands.CommandNotFound):
         return  # fuck that
     elif isinstance(error, NoTimezonesFoundError):
@@ -129,5 +137,7 @@ async def on_command_error(ctx, error):
             description="<generic error message here> - you screwed something up.")
 
     await ctx.send(embed=embed)
+    raise error
+
 
 bot.run(aevum_auth.TOKEN)
